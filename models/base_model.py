ab61b931feff026d8816a9a72362169a2624b40a
seObando19 <sebastiano-10@hotmail.com>
2020-10-30 19:27:57 +0000

    Updated file added the function [__init__][__str__][save]

--- models/base_model.py
+++ models/base_model.py
@@ -5,3 +5,18 @@ from datetime import datetime
 
 class BaseModel:
     '''Base Model class for the proyect'''
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid4())
+        self.created_at = datetime.today().isoformat()
+        self.updated_at = datetime.today().isoformat()
+
+    def __str__(self):
+        ''''''
+        text = "[{}] ({}) {}"
+        return text.format(__name__, self.id, self.__dict__)
+
+    def save(self):
+        '''
+        updates the public instance attribute updated_at
+        with the current datetime
+        '''
