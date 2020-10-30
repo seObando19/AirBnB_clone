82542b994678d141f2b02dbb0429ac4f08112b10
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-10-30 11:12:12 -0500

    added basemodel

---
+++ models/base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''this module is the base class for all other classes'''
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    '''Base Model class for the proyect'''
