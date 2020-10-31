31c682f15f30c64ce3e5b9b88289c545ea917c3f
seObando19 <sebastiano-10@hotmail.com>
2020-10-31 18:59:37 +0000

    [Updated v1] base_model with all functionally with method [save][to_dict]

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
 '''this module is the base class for all other classes'''
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime, date, time
 
 class BaseModel:
     '''Base Model class for the proyect'''
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
+        dt = datetime.now()
+
         self.id = str(uuid4())
-        self.created_at = datetime.today().isoformat()
-        self.updated_at = datetime.today().isoformat()
+        """
+        dt.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        """
+        self.created_at = dt
+        self.updated_at = dt
 
     def __str__(self):
         ''''''
         text = "[{}] ({}) {}"
-        return text.format(__name__, self.id, self.__dict__)
+        return text.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         '''
         updates the public instance attribute updated_at
         with the current datetime
         '''
+        dt = datetime.now()
+
+        self.updated_at = dt
+
+    def to_dict(self):
+        '''
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        '''
+        self.__dict__.update(
+            {"updated_at" : self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")}
+            )
+        self.__dict__.update(
+            {"created_at" : self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")}
+        )
+        self.__dict__['__class__'] = self.__class__.__name__
+        return dict(self.__dict__)
