fe0aed88d02e5aeb678c3ca28898c77cb73cf976
seObando19 <sebastiano-10@hotmail.com>
2020-11-03 14:33:05 +0000

    [Updated] file file_storage for theme the code style and pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,34 +7,32 @@ import os
 
 class FileStorage:
     '''class for serialization and deserialization'''
-    __file_path = "file.json" ##this have to be a string variable type
-    __objects = {} ### disctionary type object
-
+    ''''this have to be a string variable type'''
+    __file_path = "file.json"
+    ''''disctionary type object'''
+    __objects = {}
 
     def all(self):
         '''returns objects'''
         return self.__objects
-
 
     def new(self, obj):
         '''method to set new object in __object vriable'''
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
-
     def save(self):
         '''method save'''
         with open(self.__file_path, mode="w", encoding="utf-8") as my_file:
             __dic = {k: v.to_dict() for k, v in self.__objects.items()}
-            ##Si no chequea bien hay que hacer sort al JSON
+            '''Si no chequea bien hay que hacer sort al JSON'''
             json.dump(__dic, my_file, indent=4, default=str)
+            ''' for k, v in __mydic.items():
+                print("{}".format(k))
+                print("VALUE {}".format(v))'''
 
-
-                ##for k, v in __mydic.items():
-                  ##  print("{}".format(k))
-                    ##print("VALUE {}".format(v))
     def reload(self):
-        '''Reload method''' 
+        '''Reload method'''
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, mode="r", encoding="utf-8") as my_file:
                 dic_to_dic = json.load(my_file)
