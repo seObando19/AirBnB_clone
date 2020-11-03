9f01b6c58a9c75f7c1b0ae9a07f01978693f3d85
seObando19 <sebastiano-10@hotmail.com>
2020-11-03 17:40:03 +0000

    [Updated] pep8 style

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-'''this module is the base class for all other classes'''
+"""
+Define  BaseModel class
+"""
 from uuid import uuid4
 from datetime import datetime, date, time
 import models
 
 
-def time_conversor(obj):
-    """ Define time conversor
-        that return new time object
-    """
-    if type(obj) in [datetime]:
-        obj = obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
-    return datetime.strptime(obj, "%Y-%m-%dT%H:%M:%S.%f")
-
-
 class BaseModel:
-    '''Base Model class for the proyect'''
+    """
+        Base Model class for the proyect
+    """
     def __init__(self, *args, **kwargs):
         if kwargs:
             self.created_at = time_conversor(kwargs["created_at"])
@@ -32,23 +27,25 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        ''''''
+        """
+           String representation of BaseModel class
+        """
         text = "[{}] ({}) {}"
         return text.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        '''
-        updates the public instance attribute updated_at
-        with the current datetime
-        '''
+        """
+            updates the public instance attribute updated_at
+            with the current datetime
+        """
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        '''
-        Returns a dictionary containing all keys/values of __dict__
-        of the instance
-        '''
+        """
+            Returns a dictionary containing all keys/values of __dict__
+            of the instance
+        """
         if type(self.created_at) in [str]:
             self.created_at = time_conversor(self.created_at)
         if type(self.updated_at) in [str]:
@@ -58,3 +55,13 @@ class BaseModel:
         dictionary = (self.__dict__).copy()
         dictionary['__class__'] = self.__class__.__name__
         return dictionary
+
+
+def time_conversor(obj):
+    """
+        Define time conversor
+        that return new time object
+    """
+    if type(obj) in [datetime]:
+        obj = obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    return datetime.strptime(obj, "%Y-%m-%dT%H:%M:%S.%f")
