fc1e4575cfd4cb7f8060a8b6e7ec79b5b3e8c2c1
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-11-03 20:43:12 -0500

    almost done proyect, point 8

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,21 +2,19 @@
 """
 Test FileStorage class
 """
-
-
 import unittest
-# import pep8
+import pep8
 import json
 import os
 import shutil
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 """from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review"""
-from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
